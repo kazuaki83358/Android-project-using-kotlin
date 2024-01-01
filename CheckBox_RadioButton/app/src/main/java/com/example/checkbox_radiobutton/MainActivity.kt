fcaebe7ab1cedd45f96cdbaecb3fa0d0927152a5
Nishant Kumar <nishant.kumar83358@gmail.com>
2024-01-01 12:44:56 +0530

    project

--- CheckBox_RadioButton/app/src/main/java/com/example/checkbox_radiobutton/MainActivity.kt
+++ CheckBox_RadioButton/app/src/main/java/com/example/checkbox_radiobutton/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.checkbox_radiobutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.RadioButton
@@ -13,37 +14,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var CheckBox1 : CheckBox = findViewById(R.id.checkbox1)
-        var CheckBox2 : CheckBox = findViewById(R.id.checkbox2)
-        var CheckBox3 : CheckBox = findViewById(R.id.checkbox3)
-        val Text_View : TextView = findViewById(R.id.textView3)
-        val Confirm_Order : Button = findViewById(R.id.Order)
-        val RadioGroup : RadioGroup = findViewById(R.id.food_selection)
-        val Burgger : RadioButton = findViewById(R.id.burgger)
-        val Pizza : RadioButton = findViewById(R.id.pizza)
-        val Momos : RadioButton = findViewById(R.id.momos)
-        var Food = ""
-        var Drink = ""
-        //working with radio group
-        Pizza.setOnClickListener {
-            Burgger.isChecked = false
-            Momos.isChecked = false
-            Food = " Pizza "
-        }
-        Momos.setOnClickListener(){
-            Burgger.isChecked = false
-            Pizza.isChecked = false
-            Food = " Momos "
-        }
-        Burgger.setOnClickListener(){
-            Pizza.isChecked = false
-            Momos.isChecked = false
-            Food = " burgger "
-        }
-        //taking order and displaying order after clicking confirm order
-        Confirm_Order.setOnClickListener {
-            Toast.makeText(this,"Your order has been confirmed ",Toast.LENGTH_LONG).show()
+        val textView :TextView = findViewById(R.id.textView3)
+        val btn : Button = findViewById(R.id.Order)
+        val food_selection : RadioGroup = findViewById(R.id.food_selection)
+        val coco_cola : CheckBox = findViewById(R.id.checkbox1)
+        val pepsi : CheckBox = findViewById(R.id.checkbox2)
+        val fanta : CheckBox = findViewById(R.id.checkbox3)
+        btn.setOnClickListener{
+            val checkedFoodRadioButtonId = food_selection.checkedRadioButtonId
+
+            if (checkedFoodRadioButtonId != View.NO_ID) {
+                val food = findViewById<RadioButton>(checkedFoodRadioButtonId)?.text ?: ""
+
+                val drink1 = coco_cola.isChecked
+                val drink2 = pepsi.isChecked
+                val drink3 = fanta.isChecked
+
+                val orderString = "You ordered:\n$food" +
+                        (if (drink1) "\nCoco Cola" else "") +
+                        (if (drink2) "\nPepsi" else "") +
+                        (if (drink3) "\nFanta" else "")
+
+                textView.text = orderString
+            } else {
+                Toast.makeText(this, "Please select a food item", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
-}
+
+    }
