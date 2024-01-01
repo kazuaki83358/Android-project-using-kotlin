417de23dfa147c62ed2fd37a60142ce9759d6e7e
Nishant Kumar <nishant.kumar83358@gmail.com>
2024-01-01 13:50:43 +0530

    chekbox and radiogroup

--- CheckBox_RadioButton/app/src/main/java/com/example/checkbox_radiobutton/MainActivity.kt
+++ CheckBox_RadioButton/app/src/main/java/com/example/checkbox_radiobutton/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.checkbox_radiobutton
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,38 +10,70 @@ import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //text view
         val textView :TextView = findViewById(R.id.textView3)
+        //button
         val btn : Button = findViewById(R.id.Order)
-        val food_selection : RadioGroup = findViewById(R.id.food_selection)
-        val coco_cola : CheckBox = findViewById(R.id.checkbox1)
-        val pepsi : CheckBox = findViewById(R.id.checkbox2)
-        val fanta : CheckBox = findViewById(R.id.checkbox3)
-        btn.setOnClickListener{
-            val checkedFoodRadioButtonId = food_selection.checkedRadioButtonId
+        //checkbox
+        val checkbox1: CheckBox = findViewById(R.id.checkbox1)
+        val checkbox2: CheckBox = findViewById(R.id.checkbox2)
+        val checkbox3: CheckBox = findViewById(R.id.checkbox3)
+        //radio group
+        val radio1: RadioButton = findViewById(R.id.burgger)
+        val radio2: RadioButton = findViewById(R.id.pizza)
+        val radio3: RadioButton = findViewById(R.id.momos)
+        val radioGroup : RadioGroup = findViewById(R.id.food_selection)
+        //Empty String
+        var food = ""
 
-            if (checkedFoodRadioButtonId != View.NO_ID) {
-                val food = findViewById<RadioButton>(checkedFoodRadioButtonId)?.text ?: ""
-
-                val drink1 = coco_cola.isChecked
-                val drink2 = pepsi.isChecked
-                val drink3 = fanta.isChecked
-
-                val orderString = "You ordered:\n$food" +
-                        (if (drink1) "\nCoco Cola" else "") +
-                        (if (drink2) "\nPepsi" else "") +
-                        (if (drink3) "\nFanta" else "")
-
-                textView.text = orderString
-            } else {
-                Toast.makeText(this, "Please select a food item", Toast.LENGTH_SHORT).show()
-            }
+        radio1.setOnClickListener{
+            radio2.isChecked = false
+            radio3.isChecked = false
+            food = radio1.text.toString()
         }
+        radio2.setOnClickListener{
+            radio1.isChecked = false
+            radio3.isChecked = false
+            food = radio2.text.toString()
+        }
+        radio3.setOnClickListener{
+            radio1.isChecked = false
+            radio2.isChecked = false
+            food = radio3.text.toString()
+        }
+        //button on click listener
+        btn.setOnClickListener{
+            // Get the state of each checkbox
+            val isCheckedCheckbox1: Boolean = checkbox1.isChecked
+            val isCheckedCheckbox2: Boolean = checkbox2.isChecked
+            val isCheckedCheckbox3: Boolean = checkbox3.isChecked
 
+            val selectedCheckBox = StringBuilder()
+
+            if (isCheckedCheckbox1) {
+                selectedCheckBox.append("Coco Cola ")
+            }
+
+            if (isCheckedCheckbox2) {
+                selectedCheckBox.append("Pepsi ")
+            }
+
+            if (isCheckedCheckbox3) {
+                selectedCheckBox.append("Fanta ")
+            }
+
+            val result = selectedCheckBox.toString().trim()
+            textView.setText("Your order is :\n$food \n$result")
+            //adding toast
+            Toast.makeText(this,"Your order has been confirmed",Toast.LENGTH_LONG).show()
+        }
     }
 
     }
