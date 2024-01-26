a93efa4657391bd8cefe829980a8a50a5ab8c3d5
Nishant Kumar <nishant.kumar83358@gmail.com>
2024-01-26 20:53:19 +0530

    login and signup app

--- LoginAndSignUp/app/src/main/java/com/example/loginandsignup/MainActivity.kt
+++ LoginAndSignUp/app/src/main/java/com/example/loginandsignup/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.loginandsignup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.loginandsignup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.backBtn.setOnClickListener(){
+            Intent(this,WelcomeActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
+        binding.reSignup.setOnClickListener(){
+            Intent(this,SignUpActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
     }
 }
