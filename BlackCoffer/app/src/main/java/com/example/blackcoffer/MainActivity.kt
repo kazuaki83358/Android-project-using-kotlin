0be8ab32624820d22ed8d33d3b60831164ffdae5
Nishant kumar <nishant.kumar83358@gmail.com>
2024-05-16 14:04:29 +0530

    BlackCoffer App

---
+++ BlackCoffer/app/src/main/java/com/example/blackcoffer/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.blackcoffer
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            Intent(this,ExploreActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        },1000)
+    }
+}
